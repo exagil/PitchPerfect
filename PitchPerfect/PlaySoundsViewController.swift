f50f015729d9670d784d6fdba8607a8fe0932730
Chirag <me@chi6rag.net>
2016-12-19 21:35:30 +0530

    Adding button callbacks for play and stop buttons

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -3,6 +3,13 @@ import UIKit
 class PlaySoundsViewController: UIViewController {
 
     var recordedAudioUrl : URL!
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
 
     override func viewWillAppear(_ animated: Bool) {
         print("PlaySoundsViewController: " + recordedAudioUrl.absoluteString  )
@@ -18,14 +25,11 @@ class PlaySoundsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func stopButtonPressed(_ sender: Any) {
+        print("stopButtonPressed")
     }
-    */
 
+    @IBAction func playSoundForButton(_ sender: Any) {
+        print("playSoundForButton")
+    }
 }
